28cd5aa6a1d6767d00a20a77163cc01ee952f0db
letalandroid <carlossoncra@gmail.com>
2025-03-10 21:10:29 -0500

    Agregar configuración de Tailwind CSS y componentes iniciales

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js,astro}'],
+    theme: {
+      colors: {
+        'primary': '#006A07',
+        'green-light': '#C1FE74'
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+      },
+      extend: {
+      }
+    },
+  }
