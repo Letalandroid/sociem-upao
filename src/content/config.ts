552133d0f75ea224124971559f8cf21a7ef859b3
Letalandroid <carlossoncra@gmail.com>
2025-04-06 10:25:25 -0500

    Agregar importación de glob y ajustar la colección de sesiones para mejorar la configuración del contenido.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,3 +1,4 @@
+import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const courses = defineCollection({
@@ -11,12 +12,16 @@ const courses = defineCollection({
 });
 
 const sessions = defineCollection({
+  // loader: glob({
+  //   pattern: "./**/*.md",
+  //   base: "./src/content/sessions/",
+  // }),
   schema: z.object({
     id: z.number(),
     img: z.string(),
     course: z.string(),
-    video: z.string()
-  })
-})
+    video: z.string(),
+  }),
+});
 
 export const collections = { courses, sessions };
